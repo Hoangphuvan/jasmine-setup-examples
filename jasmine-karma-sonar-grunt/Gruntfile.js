3e944005e3d278b9beb40639983b7b4a8e0eca40
Albert Hirawan <albg1420@gmail.com>
2014-03-11 22:22:22 +1100

    added jasmine-karma-sonar-grunt
    added examples for using grunt to run jasmine, karma & saga

---
+++ jasmine-karma-sonar-grunt/Gruntfile.js
@@ -0,0 +1,104 @@
+module.exports = function(grunt) {
+	var $srcFiles = 'src/main/javascript/**/*.js';
+	var $testFiles = 'src/test/javascript/*Test.js';
+	var $outputDir = 'target'
+	var $junitResults = $outputDir + '/junit-test-results.xml';
+	var $jasmineSpecRunner = $outputDir + '/_SpecRunner.html';
+	var $coverageOutputDir = $outputDir + '/coverage';
+	var $coverageResults = $coverageOutputDir + '/PhantomJS 1.9.7 (Mac OS X)/lcov.info';
+	var $sonarSources =	makeSonarSourceDirs($srcFiles);
+	
+	/*
+	 * Create sonar source object for each directory of source file pattern.
+	 */
+	function makeSonarSourceDirs($filesPattern) {
+		var $path = require('path');
+		var $dirs = [];
+		
+		grunt.file.expand(
+			{
+				filter: function($filePath) {
+					$dirs.push({
+						path: $path.dirname($filePath),
+						prefix: '.',	// path prefix in lcov.info
+						coverageReport: $coverageResults,
+						testReport: $junitResults
+					});
+				}
+			},
+			$filesPattern
+		);
+		
+		return $dirs;
+	}
+	
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		
+		// Jasmine test
+		jasmine: {
+			pivotal: {
+				src: $srcFiles,
+				options: {
+					specs: $testFiles,
+					outfile: $jasmineSpecRunner,
+					keepRunner: 'true'	// keep SpecRunner/outfile file
+				}
+			}
+		},
+		
+		// coverage using Karma
+		karma: {
+			continuous: {
+				singleRun: 'true',
+				browsers: [ 'PhantomJS' ]
+			},
+			
+			options: {
+				plugins: [
+					'karma-jasmine',
+					'karma-phantomjs-launcher',
+					'karma-junit-reporter',
+					'karma-coverage'
+				],
+				frameworks: [ 'jasmine' ],
+				files: [ $srcFiles, $testFiles ],
+				reporters: [ 'junit', 'coverage' ],
+			    junitReporter: {
+			      outputFile: $junitResults
+			    },
+				preprocessors: {
+					'src/main/javascript/**/*.js': [ 'coverage' ]
+				},
+				coverageReporter: {
+					type: 'lcov',
+					dir: $coverageOutputDir
+				}
+			}
+		},
+		
+		// export Karma coverage to SonarQube
+		karma_sonar: {
+			your_target: {
+				// properties for SonarQube dashboard
+				project: {
+					key: 'net.ahexample:ahexample-jasmine-karma-sonar',
+					name: 'Jasmine with Karma and SonarQube Example',
+					version: '0.0.1'
+				},
+				sources: $sonarSources
+			}
+		},
+		
+		clean: [ $outputDir ]
+	});
+	
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-jasmine');
+	grunt.loadNpmTasks('grunt-karma');
+	grunt.loadNpmTasks('grunt-karma-sonar');
+
+	grunt.registerTask('test', [ 'jasmine', 'karma:continuous' ]);
+	grunt.registerTask('sonar', [ 'karma_sonar' ]);
+	grunt.registerTask('default', 'test');
+}
